ec391d8613e75e62b2db935119ff286e4f81f832
Anushka Paliwal <paliwal.anushka@gmail.com>
2018-01-28 09:35:47 -0500

    Added end game - time's up!

--- FaceOff/Assets/EmojiFall.cs
+++ FaceOff/Assets/EmojiFall.cs
@@ -9,18 +9,40 @@ public class EmojiFall : MonoBehaviour {
 	public GameObject emojiInstance; 
 	public Sprite [] emojiSprite;
 	private int position_index = 0;
+	Text timesUpAnim;
+	Text perfectAnim;
 
 
 
 	// Use this for initialization
 	void Start () {
 		//float randTime = Random.Range (1, 3);
+		AudioListener.pause = false;
+		StartCoroutine ("Countdown", 30);
 		InvokeRepeating ("createEmoji", 5, Random.Range(1, 3));
+		timesUpAnim= GameObject.Find ("timesUp").GetComponent<Text> ();
+		timesUpAnim.enabled = false;
 	
 	}
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.down * Time.deltaTime);
+	}
+
+	private IEnumerator Countdown(int time){
+		while (time > 0) {
+			Debug.Log (time--);
+			yield return new WaitForSeconds (1);
+		}
+		if (time == 0) {
+			CancelInvoke ();
+			perfectAnim= GameObject.Find ("textPerfect").GetComponent<Text> ();
+			perfectAnim.enabled = false;
+			timesUpAnim.enabled = true;
+			timesUpAnim.GetComponent<Animation> ().Play();
+			AudioListener.pause = true;
+			Debug.Log ("Time's up!");
+		}
 	}
 
 	void createEmoji(){
