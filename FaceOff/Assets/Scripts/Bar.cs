11aa17942060182a4541fd0011bba749c0ba4043
Lucia <lucia.okeh@gmail.com>
2018-01-27 21:00:44 -0500

    working on multi-player

--- FaceOff/Assets/Scripts/Bar.cs
+++ FaceOff/Assets/Scripts/Bar.cs
@@ -8,6 +8,7 @@ public class Bar : MonoBehaviour {
 	public GameObject placeholder; 
 	private Rigidbody2D expressionEmoji;
 	private PlayerEmotions result;
+	private Score playerScore; 
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,15 @@ public class Bar : MonoBehaviour {
 		y2 = -5.34;
 		expressionEmoji = placeholder.GetComponent<Rigidbody2D>();
 		result = GetComponent<PlayerEmotions>();
+		playerScore = GameObject.FindGameObjectWithTag("score").GetComponent<Score>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (expressionEmoji.position.y < y1 && expressionEmoji.position.y > y2)
-			Debug.Log ("It's in bounds! It's" + result.getResult());
-
+		if (expressionEmoji.position.y < y1 && expressionEmoji.position.y > y2) {
+			Debug.Log ("It's in bounds! It's" + result.getResult ());
+			playerScore.addScore (10);
+		}
 		//else
 		//	Debug.Log ("It's not :(.");
 	}
